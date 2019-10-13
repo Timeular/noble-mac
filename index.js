5ca23c35efb4b3cd97e3c58e5635e5e04e14f4ed
Shane Church <shane@s-church.net>
2019-10-12 19:01:58 -0500

    Fixed require for @abandonware/noble

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const os = require('os');
 
 // noble-mac acts as a shim to noble.
 if (os.platform() === 'darwin') {
-	const Noble = require('noble/lib/noble');
+	const Noble = require('@abandonware/noble/lib/noble');
 	const macBindings = require('./lib/binding.js');
 	var nobleInstance = new Noble(macBindings);
 	module.exports = nobleInstance;
 } else {
-	module.exports = require('noble');
+	module.exports = require('@abandonware/noble');
 }
