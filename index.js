0c46a4d5cbd3150334445fa6d97eb3213246ec8a
Muhammad Yawar Ali <yawarali.sayyed@gmail.com>
2020-05-22 04:41:06 +0500

    cleanup

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const os = require('os');
 
 // noble-mac acts as a shim to noble.
-if (os.platform() === 'mac') {
+if (os.platform() === 'darwin') {
 	const Noble = require('noble/lib/noble');
 	const macBindings = require('./lib/binding.js');
 	var nobleInstance = new Noble(macBindings);
--- install.js
+++ install.js
@@ -1,7 +1,7 @@
 const os = require('os');
 var spawn = require('cross-spawn');
 
-if (os.platform() === 'mac') {
+if (os.platform() === 'darwin') {
     spawn.sync('npm', ['run', 'native_install'], {
         input: 'MacOS detected. Installing native module.',
         stdio: 'inherit'
